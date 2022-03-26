f46cf5d6b73bfd3e307bac9fb12262e1b8aef891
jeetgalani <jeetgalani@gmail.com>
2022-03-26 15:00:22 -0400

    Added characters and attributes

--- nft_game/scripts/run.js
+++ nft_game/scripts/run.js
@@ -1,11 +1,19 @@
+
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
-    const gameContract = await gameContractFactory.deploy();
+    const gameContract = await gameContractFactory.deploy(
+      ["Mario", "Luigi", "Peach"],       // Names
+      ["https://i.imgur.com/COpBEoN.png", // Images
+      "https://i.imgur.com/iekHvmd.png", 
+      "https://i.imgur.com/7HxqJK8.png"],
+      [100, 200, 300],                    // HP values
+      [100, 50, 25]                       // Attack damage values
+    );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
   };
   
-const runMain = async () => {
+  const runMain = async () => {
     try {
       await main();
       process.exit(0);
@@ -13,7 +21,7 @@ const runMain = async () => {
       console.log(error);
       process.exit(1);
     }
-};
+  };
   
-runMain();
+  runMain();
   
